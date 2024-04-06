a8c8903194c521f4c72bf00dcb6f756fe1220341
S-a-b-a-r-i-s-h <sabrusabarish@gmail.com>
2024-04-06 12:05:51 +0530

    about changes

--- components/ui/About.tsx
+++ components/ui/About.tsx
@@ -11,13 +11,13 @@ const About = () => {
     })
   }) 
   return (
-    <div className="w-[100vw] border-2 flex flex-col items-center justify-center">
-      <div className="md:w-10-/12 w-9/12">
+    <div className="w-[100vw] flex flex-col items-center justify-center mt-24">
+      <h1 className='text-[50px] opacity-0 sm:text-[80px] md:text-[100px] lg:text-[160px] leading-tight h-[20vh] max-sm:h-[10vh] text-slate-300 incepto'>INCEPTO</h1>
+      <div className="max:md:w-[100]">
         <video src="/assets/logoanimate.mp4" className="pointer-events-none w-full" autoPlay muted playsInline={true} key="logoanimate">
           {/* <source src="/assets/logoanimate" type="mp4" /> */}
         </video>
       </div>
-      <h1 className='text-[50px] opacity-0 sm:text-[80px] md:text-[100px] lg:text-[160px] leading-tight h-[20vh] max-sm:h-[10vh] text-slate-300 incepto'>INCEPTO</h1>
     </div>
   )
 }
