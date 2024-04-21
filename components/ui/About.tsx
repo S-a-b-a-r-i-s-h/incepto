ad8a23d18f7c255c883b32f6ba0b23d9a8c4f2af
S-a-b-a-r-i-s-h <sabrusabarish@gmail.com>
2024-04-21 10:34:42 +0530

    position changes of video

--- components/ui/About.tsx
+++ components/ui/About.tsx
@@ -14,10 +14,11 @@ const About = () => {
   }) 
   return (
     <div className="w-[100vw]  flex flex-col items-center justify-center ">
-      <div className=" max-md:w-[100vw] w-[40vw]">
-        <video src="/assets/logoanimate.mp4" className="pointer-events-none w-full"  autoPlay muted playsInline={true} key="logoanimate">
+      <div className=" max-md:w-[100vw] w-[40vw] flex">
+        <video src="/assets/logoanimate.mp4" className="pointer-events-none w-[85%] mr-6 max-lg:w-[90vw]"  autoPlay muted playsInline={true} key="logoanimate">
           {/* <source src="/assets/logoanimate" type="mp4" /> */}
         </video>
+        {/* <div className="border-2 border-red-500 w-[50vw]"></div> */}
       </div>
       <h1 className='text-[50px] opacity-0 sm:text-[80px] md:text-[100px] lg:text-[160px] leading-tight h-[20vh] max-sm:h-[10vh] bg-gradient-to-r from-zinc-500 via-zinc-600 to-zinc-700 bg-clip-text text-transparent incepto'>INCEPTO</h1>
       <p className="text-slate-300 text-xl font-bold">09 - 10 April, 2024</p>
