032bc07d23df3dea8b6a9a491bb2c76ab82d6c72
S-a-b-a-r-i-s-h <sabrusabarish@gmail.com>
2024-04-26 12:46:58 +0530

    og change

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,15 +12,7 @@ const dmSans = DM_Sans({
 export const metadata: Metadata = {
   title: "INCEPTO",
   description: "A nation-wide 24 hour hackathon happening at College of Engineering Perumon. It aims to provide a platform for innovative minds to unveil their technical and problem solving skills being alongside like minded individuals on a marathon spirit.",
-  openGraph:{
-    title: "INCEPTO",
-    description: "A nation-wide 24 hour hackathon happening at College of Engineering Perumon. It aims to provide a platform for innovative minds to unveil their technical and problem solving skills being alongside like minded individuals on a marathon spirit.",
-    images:[
-      {
-        url: "./opengraph-image.png"
-      }
-    ]
-  }
+  
 };
 
 export default function RootLayout({
